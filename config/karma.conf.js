1ee9fbe0f70473b5526b9889816261d17b979844
Jonas Van der Biest <jvanderbiest@gmail.com>
2016-05-27 22:37:42 +0200

    add missing packages

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -60,7 +60,7 @@ module.exports = function (config) {
   };
 
   if (process.env.TRAVIS) {
-    configuration.browsers = ['Chrome_travis_ci'];
+    configuration.browsers = ['PhantomJS'];
   }
 
   config.set(configuration);
